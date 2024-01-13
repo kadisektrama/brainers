8b8175dda95c64d6869832e89cfe0d258221ffb8
ZaremskiUladzislau <grazhdanskijv@mail.ru>
2024-01-13 18:09:30 +0300

    webpack/babel

---
+++ public/locales/ru.ts
@@ -0,0 +1,11 @@
+export let ru = {
+  common: {
+    hello: "Привет",
+  },
+  about: {
+    hello: "Привет",
+  },
+  contacts: {
+    hello: "Привет",
+  },
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,109 +1,149 @@
-import webpack from 'webpack';
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import WebpackDevServer from 'webpack-dev-server';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import WebpackBundleAnalyzer from 'webpack-bundle-analyzer';
-import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import ReactRefreshTypeScript from 'react-refresh-typescript';
+import webpack from "webpack";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import WebpackBundleAnalyzer from "webpack-bundle-analyzer";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import ReactRefreshTypeScript from "react-refresh-typescript";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import WebpackDevServer from "webpack-dev-server";
 
 interface Env {
-    mode?: 'development' | 'production'
+  mode?: "development" | "production";
 }
 
 export default (env: Env) => {
-    let isDev = env.mode === 'development';
-    let mode = env.mode || 'development';
+  let isDev = env.mode === "development";
+  let mode = env.mode || "development";
 
-    let moduleCssLoader = {
-        loader: 'css-loader',
-        options: {
-            modules: true
-        },
-    }
+  let moduleCssLoader = {
+    loader: "css-loader",
+    options: {
+      modules: true,
+    },
+  };
 
-    const config: webpack.Configuration = {
-        entry: {
-            main: path.resolve(__dirname, 'src', 'index.tsx'),
-        },   
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].[hash].js',
-            clean: true,
+  const config: webpack.Configuration = {
+    entry: {
+      main: path.resolve(__dirname, "src", "index.tsx"),
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[hash].js",
+      clean: true,
+    },
+    mode: mode,
+    plugins: [
+      !isDev &&
+        new MiniCssExtractPlugin({
+          filename: "[name].[hash].css",
+          chunkFilename: "[name].[hash].[id].css",
+        }),
+      new HtmlWebpackPlugin({
+        title: "Brainers",
+        template: path.resolve(__dirname, "public", "index.html"),
+        favicon: path.resolve(__dirname, "public", "favicon.ico"),
+      }),
+      isDev && new webpack.ProgressPlugin(),
+      !isDev &&
+        new WebpackBundleAnalyzer.BundleAnalyzerPlugin({
+          analyzerMode: "static",
+        }),
+      isDev && new ReactRefreshPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "public", "locales"),
+            to: path.resolve(__dirname, "dist", "locales"),
+          },
+        ],
+      }),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            moduleCssLoader,
+            "sass-loader",
+          ],
         },
-        mode: mode,
-        plugins: [
-            !isDev && new MiniCssExtractPlugin({
-                filename: '[name].[hash].css',
-                chunkFilename: '[name].[hash].[id].css',
-            }),
-            new HtmlWebpackPlugin({
-                title: 'Brainers',
-                template: path.resolve(__dirname, 'public', 'index.html'),
-            }),
-            isDev && new webpack.ProgressPlugin(),
-            !isDev && new WebpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: 'static' }),
-            isDev && new ReactRefreshPlugin()
-        ].filter(Boolean),
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, moduleCssLoader, "sass-loader"],
-                },
-                {
-                    test: /\.css$/i,
-                    use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, moduleCssLoader],
-                },
-                // {
-                //     test: /\.tsx?$/,
-                //     use: 'ts-loader',
-                //     exclude: /node_modules/,
-                // },
-                {
-                    test: /\.[jt]sx?$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: require.resolve('ts-loader'),
-                            options: {
-                                getCustomTransformers: () => ({
-                                    before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
-                                }),
-                                transpileOnly: isDev,
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif)$/i,
-                    type: 'asset/resource',
-                },
-                {
-                    test: /\.svg$/i,
-                    issuer: /\.[jt]sx?$/,
-                    use: ['@svgr/webpack'],
-                },
-            ],
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            moduleCssLoader,
+          ],
         },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-            alias: {
-                '@': path.resolve(__dirname, 'src'),  
+        // {
+        //     test: /\.tsx?$/,
+        //     use: 'ts-loader',
+        //     exclude: /node_modules/,
+        // },
+        // {
+        //   test: /\.[jt]sx?$/,
+        //   exclude: /node_modules/,
+        //   use: [
+        //     {
+        //       loader: require.resolve("ts-loader"),
+        //       options: {
+        //         getCustomTransformers: () => ({
+        //           before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+        //         }),
+        //         transpileOnly: isDev,
+        //       },
+        //     },
+        //   ],
+        // },
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                [
+                  "@babel/preset-react",
+                  { runtime: isDev ? "automatic" : "classic" },
+                ],
+                "@babel/preset-typescript",
+              ],
             },
+          },
         },
-        devtool: isDev ? 'inline-source-map' : false,
-        devServer: isDev ? {
-            // static: {
-            //     directory: path.join(__dirname, 'public'),
-            // },
-            // compress: true,
-            port: 5000,
-            open: true,
-            historyApiFallback: true,
-            hot: true,
-        } : undefined
-    }
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ["@svgr/webpack"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: isDev
+      ? {
+          // static: {
+          //     directory: path.join(__dirname, 'public'),
+          // },
+          // compress: true,
+          port: 5000,
+          open: true,
+          historyApiFallback: true,
+          hot: true,
+        }
+      : undefined,
+  };
 
-    return config
+  return config;
 };
